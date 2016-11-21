b0be5489dc3feb25f1d627fd6248246254540b07
Jitlada Supreeyaporn <jitlada.su@ku.th>
2016-11-21 22:20:29 +0700

    encrypt message with public key and send to server, then receive cipher text from server and decrypt with private key

--- client.py
+++ client.py
@@ -1,5 +1,12 @@
 import socket
 import sys
+from Crypto.PublicKey import RSA
+
+#def binToHex(str):
+#    return binascii.hexlify(str)
+
+#def hexToBin(str):
+#    return binascii.dehexlify(str)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = ('158.108.34.100', 8000)
@@ -7,17 +14,26 @@ print >> sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
 try:
+    pub_key = open('../.ssh/id_rsa.pub', 'rb').read()
+    pri_key = open('../.ssh/id_rsa', 'rb').read()
+    publicKey = RSA.importKey(pub_key)
+    privateKey = RSA.importKey(pri_key)
+
     message = 'hello, world'
-    print >> sys.stderr, 'sending %s' % message
-    sock.sendall(message)
-
+    cipher = publicKey.encrypt(message, 32)[0]
+    print 'message is %s' % privateKey.decrypt(cipher)
     amount_received = 0
-    amount_expected = len(message)
+    amount_expected = len(cipher)
+    
+    print >> sys.stderr, 'sending'
+    sock.sendall(cipher)
 
+    data = ''
     while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print >> sys.stderr, 'receive "%s"' % data
+        data += sock.recv(16)
+        amount_received = len(data)
+    data = privateKey.decrypt(data)
+    print >> sys.stderr, 'receive "%s"' % data
 
 finally:
     print sys.stderr, 'closing socket'
